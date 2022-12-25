035471a176a7d242e41f387c6375e07363f20776
maxk3y <109315215+maxk3y@users.noreply.github.com>
2022-12-25 21:40:47 +0200

    1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+let interval = undefined;
+
+function startColorChange() {
+  interval = setInterval(changeBackgroundColor, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function stopColorChange() {
+  clearInterval(interval);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  document.body.style.backgroundColor = '#fff';
+}
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
